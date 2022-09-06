ccacc43eb880826efffd510d2cb5a830e0999932
Lưu Mác <nguoiemcuanui@gmail.com>
2022-09-06 17:18:08 +0700

    coverage1

--- functions.test.js
+++ functions.test.js
@@ -23,12 +23,12 @@ describe('Add two numbers', () => {
 })
 
 
-//describe('Minus two numbers', () => {
+describe('Minus two numbers', () => {
 
-//  test('Numeric inputs should give the answer', ()=>{
-//    expect(minus(8,7)).toEqual(8-7)
-  //})
-  //test('Mixing numeric inputs should give the answer', ()=>{
-   // expect(minus(9,7)).toEqual(9-7)
-  //})
-//})
+  test('Numeric inputs should give the answer', ()=>{
+    expect(minus(8,7)).toEqual(8-7)
+  })
+  test('Mixing numeric inputs should give the answer', ()=>{
+    expect(minus(9,7)).toEqual(9-7)
+  })
+})
